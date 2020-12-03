24abe26538dcf4cc6519e602ebbc1885d45df251
Abhishek149999 <51540265+Abhishek149999@users.noreply.github.com>
2020-12-03 17:14:42 +0530

    Add files via upload

---
+++ Max_profit.php
@@ -0,0 +1,41 @@
+<?php
+//To input number of Breads, vada and samosa
+echo "Enter number of Breads, Vada and Samosa: ";
+$ip1 = explode(' ', readline()); 
+
+//To input cost of vada and samosa
+echo "Enter Cost of Vada and Samosa: ";
+$ip2 = explode(' ', readline());
+
+$p=0;
+
+//Checking which product have higher cost(Samosa or Vada) and Prefering higher cost one
+if ($ip2[0]< $ip2[1]){
+		$m=$ip1[2];
+		$max_ct=$ip2[1];
+		$n=$ip1[1];
+		$min_ct=$ip2[0];
+	}
+else if($ip2[0]>=$ip2[1]){
+		$m=$ip1[1];
+		$max_ct=$ip2[0];
+		$n=$ip1[2];
+		$min_ct=$ip2[1];
+	}
+//Estimating Max Profit
+while($ip1[0]>=2){
+	if($m>0){
+		$p=$p+$max_ct;
+		$m=$m-1;
+		$ip1[0]=$ip1[0]-2;
+	}
+	else if($n>0){
+		$p=$p+$min_ct;
+		$n=$n-1;
+		$ip1[0]=$ip1[0]-2;
+	}
+}
+
+echo "Maximum Possible profit is " . $p ."<br>"; 
+
+?>
